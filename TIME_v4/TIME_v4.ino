ddcd8d36b8f3c7874c72c9a74f71fcdb01c15791
Ionut Cotoi <ionut.cotoi@opendevlabs.com>
2013-03-18 22:48:59 +0200

    WiFly and display working at the same time

--- TIME_v4/TIME_v4.ino
+++ TIME_v4/TIME_v4.ino
@@ -1,3 +1,4 @@
+#include <TimerOne.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
 #include "RTClib.h"
@@ -72,56 +73,55 @@ void load_ascii_string(char s[])
   }
 }
 
-
 void setup() 
 {       
-  /*
-  cli();
-  */
   init_display_pins();
   digitalWrite(11, HIGH); // Display off
-  /*
   init_display_refresh();
-  sei();  
   clear_screen();
-  */
+  
+
+  load_ascii_string("START...");
+  put_string(0, TEXT);
   
   char buf[32];  
   // pinMode(2, INPUT);      // This is the main button, tied to INT0
   // digitalWrite(2, HIGH);  // Enable internal pull up on button
-  Serial.begin(115200);
-  wifiSerial.begin(9600);
+  Serial.begin(9600);
+  
   //Serial.print(F("Free memory: "));
   //Serial.println(wifly.getFreeMemory(),DEC);
 
   //WiFi
-  if (!wifly.begin(&wifiSerial, &Serial)) {
-    Serial.println("Failed to start wifly");
-    terminal();
+  if (!wifly.begin(&Serial, NULL)) {
+    load_ascii_string("CANNOT");
+    scroll_text(500, TEXT);
+    //terminal();
+    
   }
 
-  /* Join wifi network if not already associated */
+  // Join wifi network if not already associated 
   if (!wifly.isAssociated()) {
-    /* Setup the WiFly to connect to a wifi network */
-    Serial.println(F("Joining network"));
+    //Serial.println(F("Joining network"));
     wifly.setSSID(mySSID);
     wifly.setPassphrase(myPassword);
     wifly.enableDHCP();
 
     if (wifly.join()) {
-      Serial.println(F("Joined wifi network"));
+      load_ascii_string("JOINED");
+      scroll_text(500, TEXT);
     } 
     else {
-      Serial.println(F("Failed to join wifi network"));
-      terminal();
+      load_ascii_string("FAILED");
+      scroll_text(500, TEXT);
     }
   } 
   else {
-    Serial.println(F("Already joined network"));
+    //Serial.println(F("Already joined network"));
   }
 
   
-  Serial.print(F("MAC: "));
+  /*Serial.print(F("MAC: "));
   Serial.println(wifly.getMAC(buf, sizeof(buf)));
   Serial.print(F("IP: "));
   Serial.println(wifly.getIP(buf, sizeof(buf)));
@@ -129,17 +129,18 @@ void setup()
   Serial.println(wifly.getNetmask(buf, sizeof(buf)));
   Serial.print(F("Gateway: "));
   Serial.println(wifly.getGateway(buf, sizeof(buf)));
-
+  */
   wifly.setDeviceID("TI:ME");
-  Serial.print(F("DeviceID: "));
-  Serial.println(wifly.getDeviceID(buf, sizeof(buf)));
+  /*Serial.print(F("DeviceID: "));
+  Serial.println(wifly.getDeviceID(buf, sizeof(buf)));*/
  
-  
   if (wifly.isConnected()) {
-    Serial.println(F("Old connection active. Closing"));
+    //Serial.println(F("Old connection active. Closing"));
     wifly.close();
   }
-
+  
+  load_ascii_string("T I : M E");
+  scroll_text(500, TEXT);
   
   // Init RTC
   pinMode(A2, OUTPUT);
@@ -155,28 +156,27 @@ void setup()
 
   // Set RTC to date & time this sketch was compiled
   RTC.adjust(DateTime(__DATE__, __TIME__));
-  Serial.println(F("Connected to wireless network!"));
   
   last_time = millis();
 }
 void parse_html()
 {
-  Serial.println(F("--Parse HTML--"));
+  //Serial.println(F("--Parse HTML--"));
  
   if (wifly.isConnected()) {
-        Serial.println("Old connection active. Closing");
+        //Serial.println("Old connection active. Closing");
 	wifly.close();
   }
  
   if (wifly.open(site, 80)) {
-        Serial.print(F("Connected to "));
-	Serial.println(site);
+        //Serial.print(F("Connected to "));
+	//Serial.println(site);
 
 	/* Send the request */
 	wifly.println("GET http://smartcam.inventeaza.ro/time/ HTTP/1.0");
 	wifly.println();
     } else {
-        Serial.println(F("Failed to connect"));
+        //Serial.println(F("Failed to connect"));
     }
     
     for(int i=0;i<BUFF_SIZE;++i) {
@@ -184,24 +184,25 @@ void parse_html()
         break;
       }
       char ch = wifly.read();
-      Serial.write(ch);
+      //Serial.write(ch);
       TEXT[i] = ch;
     }
 }
 void loop() 
 { 
-  /*put_time();
+  put_time(2000);
   // e is UNDEFINED in the lookup table
-  //load_ascii_string("T E S T   D E   A F I S A R E !");
+  load_ascii_string("T E S T   D E   A F I S A R E !");
+  scroll_text(200, TEXT);
   
-  
-  if(now.second() - last_time > WAIT_REQ) {
-    parse_html();
-    last_time = now.second();
-  }
+  //if(now.second() - last_time > WAIT_REQ) {
+  //  parse_html();
+  //  last_time = now.second();
+  //}
 
   //TEXT[7] = 3; // Draw the heart
-  scroll_text(500, TEXT);*/
+  
+  /*
   if(last_time + WAIT_REQ < millis())  {
     parse_html();
     last_time = millis();
@@ -212,7 +213,7 @@ void loop()
 	char ch = wifly.read();
 	Serial.write(ch);
 	if (ch == '\n') {
-	    /* add a carriage return */ 
+	    // add a carriage return 
 	    Serial.write('\r');
 	}
     }
@@ -220,7 +221,7 @@ void loop()
     if (Serial.available() > 0) {
 	wifly.write(Serial.read());
     }
-  
+  */
   
 }
 
